6ce537c7e855075af19e90e2fed36df9bacc6ef5
ST-PC\ST <vanhongit@gmail.com>
2019-04-13 12:42:24 +0700

    complete

--- Angular2/src/app/tutorial/tutorial.component.ts
+++ Angular2/src/app/tutorial/tutorial.component.ts
@@ -5,12 +5,18 @@ import { Component } from '@angular/core';
     selector: 'my-tutorial',
     template: `
             <h2> hello moto {{title}}</h2>
+            <h3 [class.redColor]="applyClass">def</h3>
+            <h4 [style.color]="applyStyle ? 'blue' : 'orange'">apply style </h4>
+
             <input type="text" [value]="welcome" />
 
             <img src="{{imgSrc}}" /> <br />
             <img [src]="img" />
         `,
-    styles: ['h2 {color: red}']
+    styles: [`
+                h2 {color: red}
+                .redColor {color: red}
+            `]
 })
 export class TutorialComponent {
     // interpolation
@@ -20,5 +26,9 @@ export class TutorialComponent {
     // property binding
     img = 'https://lorempixel.com/300/300';
     welcome = 'welcome to angular 2';
+
+    // binding class and stype
+    applyClass = true;
+    applyStyle = false;
 
 }
