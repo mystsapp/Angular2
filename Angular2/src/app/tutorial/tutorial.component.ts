582e5e36104560d99f64b338a23df487ff7493be
ST-PC\ST <vanhongit@gmail.com>
2019-04-13 10:10:44 +0700

    Init Commit

---
+++ Angular2/src/app/tutorial/tutorial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    // tslint:disable-next-line:component-selector
+    selector: 'my-tutorial',
+    template: '<h2> hello moto</h2>',
+    styles: ['h2 {color: red}']
+})
+export class TutorialComponent {
+    
+}
