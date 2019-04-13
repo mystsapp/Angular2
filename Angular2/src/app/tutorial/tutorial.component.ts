aad3fa20d7f078ae67ba897e035e073bc510c963
ST-PC\ST <vanhongit@gmail.com>
2019-04-13 12:14:54 +0700

    interpolation and property binding

--- Angular2/src/app/tutorial/tutorial.component.ts
+++ Angular2/src/app/tutorial/tutorial.component.ts
@@ -3,9 +3,22 @@ import { Component } from '@angular/core';
 @Component ({
     // tslint:disable-next-line:component-selector
     selector: 'my-tutorial',
-    template: '<h2> hello moto</h2>',
+    template: `
+            <h2> hello moto {{title}}</h2>
+            <input type="text" [value]="welcome" />
+
+            <img src="{{imgSrc}}" /> <br />
+            <img [src]="img" />
+        `,
     styles: ['h2 {color: red}']
 })
 export class TutorialComponent {
-    
+    // interpolation
+    title = 'abc';
+    imgSrc = 'https://lorempixel.com/300/300';
+
+    // property binding
+    img = 'https://lorempixel.com/300/300';
+    welcome = 'welcome to angular 2';
+
 }
