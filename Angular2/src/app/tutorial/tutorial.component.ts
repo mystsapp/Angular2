b46949d0b29d18333560ecca881e8c2aaa62d77d
ST-PC\ST <vanhongit@gmail.com>
2019-04-13 13:07:59 +0700

    complete

--- Angular2/src/app/tutorial/tutorial.component.ts
+++ Angular2/src/app/tutorial/tutorial.component.ts
@@ -1,34 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component ({
-    // tslint:disable-next-line:component-selector
     selector: 'my-tutorial',
     template: `
             <h2> hello moto {{title}}</h2>
-            <h3 [class.redColor]="applyClass">def</h3>
-            <h4 [style.color]="applyStyle ? 'blue' : 'orange'">apply style </h4>
-
-            <input type="text" [value]="welcome" />
-
-            <img src="{{imgSrc}}" /> <br />
-            <img [src]="img" />
-        `,
-    styles: [`
-                h2 {color: red}
-                .redColor {color: red}
-            `]
+            <button (click)="OnClick(name.value)"> click me </button>
+            <input type="text" #name />
+        `
 })
 export class TutorialComponent {
     // interpolation
     title = 'abc';
-    imgSrc = 'https://lorempixel.com/300/300';
 
-    // property binding
-    img = 'https://lorempixel.com/300/300';
-    welcome = 'welcome to angular 2';
-
-    // binding class and stype
-    applyClass = true;
-    applyStyle = false;
-
+    OnClick(value) {
+        console.log(value);
+    }
 }
