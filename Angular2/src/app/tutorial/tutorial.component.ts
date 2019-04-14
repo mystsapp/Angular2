97eaee1310c7ff2b09629c9bcf523b6e30f7099f
ST-PC\ST <vanhongit@gmail.com>
2019-04-14 11:19:09 +0700

    complete

--- Angular2/src/app/tutorial/tutorial.component.ts
+++ Angular2/src/app/tutorial/tutorial.component.ts
@@ -3,17 +3,10 @@ import { Component } from '@angular/core';
 @Component ({
     // tslint:disable-next-line:component-selector
     selector: 'my-tutorial',
-    template: `
-            <h2> hello moto {{title}}</h2>
-            <button (click)="OnClick(name.value)"> click me </button>
-            <input type="text" #name />
-        `
+    templateUrl: 'tutorial.component.html'
 })
 export class TutorialComponent {
     // interpolation
     title = 'abc';
 
-    OnClick(value) {
-        console.log(value);
-    }
 }
