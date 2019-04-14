5e61b7e581d8527aeeea618b807c6b3045a0275f
ST-PC\ST <vanhongit@gmail.com>
2019-04-14 13:00:30 +0700

    complete

--- Angular2/src/app/tutorial/tutorial.component.ts
+++ Angular2/src/app/tutorial/tutorial.component.ts
@@ -3,10 +3,22 @@ import { Component } from '@angular/core';
 @Component ({
     // tslint:disable-next-line:component-selector
     selector: 'my-tutorial',
-    templateUrl: 'tutorial.component.html'
+    templateUrl: 'tutorial.component.html',
+    styleUrls: ['tutorial.component.css']
+
 })
 export class TutorialComponent {
     // interpolation
     title = 'abc';
 
+    private cone = true;
+    private ctwo = true;
+
+    private style = 'italic';
+    private size = '60px';
+
+    toggleClass() {
+        this.cone = !this.cone;
+        this.ctwo = !this.ctwo;
+    }
 }
