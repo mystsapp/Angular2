b5e68920a4061159f143fc10b4c8c427c7d0fceb
ST-PC\ST <vanhongit@gmail.com>
2019-05-26 12:36:48 +0700

    complete

--- Angular2/src/app/employee/employee.component.ts
+++ Angular2/src/app/employee/employee.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../_services/employee.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
@@ -10,11 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EmployeeComponent implements OnInit {
   employees: any[];
-  constructor(private employeeService: EmployeeService, private alertifyService: AlertifyService, private route: ActivatedRoute) {}
+  pages = [];
+  currentPage: number;
+  constructor(private employeeService: EmployeeService,
+              private alertifyService: AlertifyService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-   this.route.data.subscribe(data => {
-     this.employees = data.employees;
-   })
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.currentPage = params.pageNumber || 1; // query variable name
+      console.log(this.currentPage);
+    });
+    this.activatedRoute.data.subscribe(data => {
+      this.employees = data.employees;
+    });
+    this.pages = [1, 2, 3, 4, 5];
   }
 }
